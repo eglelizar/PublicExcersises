cd0783ddfa288d0a6fbaf10cb151dd96de1c6e08
eglelizar <eglelizar@gmail.com>
2020-08-05 17:43:43 -0700

    Excersises

---
+++ DecodeMessage/DecodeMessage/Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecodeMessage
+{
+    class Program
+    {
+
+
+        //private static int DecoderRecursive(string str, int index, int length, int sum)
+        //{
+        //    if (index + length > str.Length) return 0;
+        //    int toEvaluate = int.Parsñe(str.Substring(index, length));           
+        //    if (toEvaluate >= 1 && toEvaluate <= 26)
+        //    {
+        //        if(index + length == str.Length){
+        //            return 1;
+        //        }
+        //        sum += DecoderRecursive(str, index+1,  length, sum) ;
+        //    }
+        //    else
+        //        return 0;
+
+        //    return sum;
+
+
+        private static int Decoder(string strToDecode)
+        {
+            int?[] memoization = new int?[strToDecode.Length + 1];
+            if (strToDecode.Length < 2) return 1;
+            int Decode1 = int.Parse(strToDecode.Substring(0, 1));
+            int Decode2 = int.Parse(strToDecode.Substring(0, 2));
+            if (Decode1 == 0 || Decode2 ==0)
+                return 0;
+            return DecoderRecursive(strToDecode, Decode1, 1, memoization) + DecoderRecursive(strToDecode, Decode2, 2, memoization);
+        }
+        private static int DecoderRecursive(string original, int Decoder, int index, int?[] memo)
+        {
+            if (Decoder == 0 || Decoder > 26)
+                return 0;
+            if (index == original.Length)
+                return 1;
+            if (memo[index].HasValue)
+            {
+                return memo[index].Value;
+            }
+            int Decode1 = int.Parse(original.ToString().Substring(index, 1));
+            int sum = DecoderRecursive(original, Decode1, index + 1, memo);
+            if (index + 2 <= original.Length)
+            {
+                int Decode2 = int.Parse(original.ToString().Substring(index, 2));
+                sum += DecoderRecursive(original, Decode2, index + 2, memo);
+            }
+            memo[index] = sum;
+            return sum;
+        }
+
+        //array 123
+        //1,2,3
+        //12,3
+        //1,23
+        //public static int DecoderRecursive(string str)
+        //{
+        //    return DecoderRecursive(str, 0,0);
+        //}
+        //private static int DecoderRecursive(string str, int index, int sum)
+        //{
+        //    if (index > str.Length) return 0;
+        //    if (index == str.Length)
+        //        return 1;
+
+        //    //Evaluate string  with length 1
+        //    if (index + 1 <= str.Length)
+        //    {
+        //        int toEvaluate1 = int.Parse(str.Substring(index, 1));
+        //        if (toEvaluate1 == 0)
+        //            return 0;
+        //        sum += DecoderRecursive(str, index + 1, sum);
+        //    }
+        //    //Evaluate string  with length 2
+        //    if (index + 2 <= str.Length)
+        //    {
+        //        int toEvaluate2 = int.Parse(str.Substring(index, 2));
+        //        if (toEvaluate2 > 26)
+        //            return 0;
+        //        sum += DecoderRecursive(str, index + 2, sum);
+        //    }
+        //    return sum;
+        //}
+
+
+        //}
+        //public static int Decoder (string original)
+        //{
+        //    return DecoderRecursive(original, 0,1) + DecoderRecursive(original, 0, 2);
+        //}
+
+        //private static int DecoderRecursive(string str,  int index, int length)
+        //{           
+        //    if (index > str.Length) return 0;
+        //    if (index == str.Length)
+        //        return 1;
+        //    int EvaluateStr = int.Parse(str.Substring(index, length));
+        //    if (EvaluateStr == 0 || EvaluateStr > 27) return 0;
+
+        //    int sum = DecoderRecursive(str, index+1, 1);
+        //    if (index + 2 <= str.Length)
+        //    {
+        //        sum += DecoderRecursive(str, index + 1, 2);
+        //    }
+        //    return sum;
+
+        //}
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter string");
+            string cad = Console.ReadLine();
+            Console.WriteLine(Decoder(cad));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TwoArraysGivesTheSumTarget/TwoArraysGivesTheSumTarget/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwoArraysGivesTheSumTarget
+{
+    class Program
+    {
+        private static int[,] result = new int[2, 3];
+        private static int resultindex = 0;
+        private static void InsertValues(int a, int b, int target, int index)
+        {
+            result[index, 0] = a;
+            result[index, 1] = b;
+            result[index, 2] = target - a - b;
+            resultindex++;
+        }
+        private static int GetIndexWithMaxDifference()
+        {
+            if (resultindex == 0 || resultindex == 1) return resultindex;
+            if (Math.Abs(result[0, 2]) > Math.Abs(result[1, 2]))
+                return 0;
+            return 1;
+        }
+        private static int GetDifference(int index)
+        {
+            if (resultindex == 0) return 0;
+            return result[index, 2];
+        }
+        private static void EvaluateToStore(int a, int b, int target)
+        {
+            int difference = target - a - b;
+            if (resultindex < 2)
+            {
+                InsertValues(a, b, target, resultindex);
+            }
+            int maxIndex = GetIndexWithMaxDifference();
+            int currentMaxDiff = GetDifference(maxIndex);
+            if (Math.Abs(currentMaxDiff) > Math.Abs(difference) && resultindex > 1)
+            {
+                InsertValues(a, b, target, maxIndex);
+            }
+
+        }
+        public static int[,]  GetValuesSum(int[] array1, int [] array2, int target)
+        {
+            //1. Get the complement 24-(-1) = 25
+            int sum = 0;
+            for (int i= 0; i < array1.Length; i++)
+                for (int j = 0; j< array2.Length; j++)
+                {                    
+                    EvaluateToStore(array1[i], array2[j], target);
+                }
+            return result;
+        }
+        //24
+        //array1 -1,3,8,2,9,5 - O(n2)
+        //array2 4,1,2,10,5,20
+        static void Main(string[] args)
+        {
+            int target = int.Parse(Console.ReadLine());
+            string str = Console.ReadLine();
+            int[] array1 = str.Split(' ').Select(x => int.Parse(x)).ToArray<int>();
+            str = Console.ReadLine();
+            int[] array2 = str.Split(' ').Select(x => int.Parse(x)).ToArray<int>();
+            int[,] result = GetValuesSum(array1, array2, target);
+            for(int i = 0; i <2; i++)
+            {
+                Console.WriteLine(string.Format("({0} {1})", result[i, 0], result[i, 1]));
+            }
+            Console.ReadKey();
+        }
+    }
+}
